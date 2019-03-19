7e4978748f86910753b44d998ca6bc38418d1d03
Ravid Cohen <ravidd9@gmail.com>
2019-03-19 16:25:00 +0200

    19.3 end day jquery

--- 19.3/jQuery/exer.js
+++ 19.3/jQuery/exer.js
@@ -1,8 +1,74 @@
-let input = $("<input id='input' placeholder='Human Name'></input>")
-$("body").append(input)
-let button = $("<button id='button'>Add Human</button>")
-$("body").append(button)
+//1 + 2
+// let input = $("<input id='input' placeholder='Human Name'></input>")
+// $("body").append(input)
+// let button = $("<button id='button'>Add Human</button>")
+// $("body").append(button)
+// $("#button").on("click", function(){
+//     $("ul").append(`<li>${input.val()}</li>`)
+// })
+// $("ul").on("click", "li", function(){
+//     $(this).remove()
+// })
 
-$("#button").on("click", function(){
-    $("ul").append("<li> ${this.</li>")
+
+//3
+// $("body").append(`<div class="box" id="b1"></div>`)
+// $("body").append(`<div class="box" id="b2"></div>`)
+// let firstRed = false
+// $("#b1").on("mouseenter", function(){
+//     if(firstRed == false){
+//         $("#b1").css("background-color", "red")
+//         $("#b2").css("background-color", "purple")
+//         firstRed= true
+//     }
+// })
+// $("#b2").on("mouseenter", function(){
+//     if(firstRed == true){
+//         $("#b2").css("background-color", "red")
+//         $("#b1").css("background-color", "purple")
+//         firstRed = false
+//     }
+// })
+
+
+//4
+let shoeCounter = 0
+let melonCounter = 0
+$(".item").on("click", function () {
+    if ($(this).attr("data-instock") == "true") {
+        let text = $(this).text()
+        if($(this).text() == "Shoe" && shoeCounter == 0){
+            $("#cart").append(`<div class="cart-item">${text}</div>`)
+            shoeCounter++
+        }
+        if($(this).text() == "Melon" && melonCounter == 0){
+            $("#cart").append(`<div class="cart-item">${text}</div>`)
+            melonCounter++
+        }
+
+
+    }
 })
+$("#cart").on("click", "div", function () {
+    $(this).remove()
+})
+
+
+//5
+// const fruits = [
+//     { name: "Orange", color: "orange" },
+//     { name: "Banana", color: "yellow" },
+//     { name: "Coconut", color: "brown" },
+//     { name: "Kiwi", color: "brown" },
+//     { name: "Lemon", color: "yellow" },
+//     { name: "Cucumber", color: "green" },
+//     { name: "Persimmon", color: "orange" },
+//     { name: "Pumpkin", color: "orange" }
+// ]
+// for (let index in fruits) {
+//     $("#basket").append(`<div class=${fruits[index].color} id=${fruits[index].name}>${fruits[index].name}</div>`)
+// }
+
+
+//6
+
